e595832b59ce9ffa0d2d6e9baf84f0f405809e78
IrinaQA423 <irina-koshina@mail.ru>
2025-07-10 11:13:41 +0800

    Добавила  константы

--- datacenter/duration_helpers.py
+++ datacenter/duration_helpers.py
@@ -1,12 +1,15 @@
 from django.utils.timezone import localtime, now
 
+SECONDS_IN_ONE_HOUR = 3600
+SECONDS_IN_ONE_MINUTE = 60
+STRANGE_VISIT_THRESHOLD = SECONDS_IN_ONE_HOUR
 
 def format_duration(duration):
     seconds = duration.total_seconds()
 
-    hours = int(seconds // 3600)
-    minutes = int((seconds % 3600) // 60)
-    seconds = int(seconds % 60)
+    hours = int(seconds // SECONDS_IN_ONE_HOUR)
+    minutes = int((seconds % SECONDS_IN_ONE_HOUR) // SECONDS_IN_ONE_MINUTE)
+    seconds = int(seconds % SECONDS_IN_ONE_MINUTE)
     return f"{hours}:{minutes:02d}:{seconds:02d}"
 
 
@@ -34,4 +37,4 @@ def is_visit_strange(duration):
 
     if not duration:
         return False
-    return duration.total_seconds() > 3600
+    return duration.total_seconds() > STRANGE_VISIT_THRESHOLD
