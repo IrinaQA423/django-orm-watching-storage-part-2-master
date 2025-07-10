e52d3616e67080e37a55c85941b1787ba6de8ce8
IrinaQA423 <irina-koshina@mail.ru>
2025-07-10 11:27:06 +0800

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
